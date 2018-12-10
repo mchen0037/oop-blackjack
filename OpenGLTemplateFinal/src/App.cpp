a51b020c43220a9f8ed4698bfb469255920099db
Mighty Chen <mchen73@ucmerced.edu>
2018-12-10 13:41:30 -0800

    cleaned up template because it was messy af

--- OpenGLTemplateFinal/src/App.cpp
+++ OpenGLTemplateFinal/src/App.cpp
@@ -2,9 +2,9 @@
 #include "App.h"
 
 App::App(int argc, char** argv): GlutApp(argc, argv){
-    explosion = new AnimatedRect("fireball.bmp", 6, 6, 100, -0.5, 0.5, 0.5, 0.5);
-    
-    fastExplosion = new AnimatedRect("fireball.bmp", 6, 6, 10, 0.5, 0.5, 0.5, 0.5);
+    explosion = new AnimatedRect("assets/fireball.bmp", 6, 6, 100, -0.5, 0.5, 0.5, 0.5);
+
+    fastExplosion = new AnimatedRect("assets/fireball.bmp", 6, 6, 10, 0.5, 0.5, 0.5, 0.5);
 }
 
 void App::draw() {
@@ -16,7 +16,7 @@ void App::keyDown(unsigned char key, float x, float y){
     if (key == 27){
         exit(0);
     }
-    
+
     if (key == ' '){
         fastExplosion->playOnce();
         explosion->playOnce();
